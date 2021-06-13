af70ba98f5a7a888b5fd5b350d365aef525ea828
Matt Blessed <matt@blessed.dev>
2021-06-13 17:15:47 -0400

    update

--- util/run-gnu-test.sh
+++ util/run-gnu-test.sh
@@ -9,7 +9,9 @@ export RUST_BACKTRACE=1
 
 if test -n "$1"; then
     # if set, run only the test passed
-    export RUN_TEST="TESTS='$1'"
+    export RUN_TEST="TESTS=\"$1\""
 fi
+
+echo "RUN_TEST: ${RUN_TEST}"
 
 timeout -sKILL 2h make -j "$(nproc)" check $RUN_TEST SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
